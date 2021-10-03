3db32bef4dd58e71d9aa59b7bd1e001e3ccb9acf
adamvassallo <avassa01@students.cs.ubc.ca>
2021-10-03 00:09:49 -0700

    lexer is complete and tested

---
+++ ProjectFiles/src/ui/Main.java
@@ -0,0 +1,21 @@
+package ui;
+
+import libs.Tokens;
+import org.antlr.runtime.MismatchedTokenException;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+import parser.MapLexer;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, MismatchedTokenException {
+        MapLexer lexer = new MapLexer(CharStreams.fromFileName("testExpressionAndConditional.tdot"));
+        for (Token token : lexer.getAllTokens()) {
+            System.out.println(token);
+        }
+        lexer.reset();
+        System.out.println("Done tokenizing");
+    }
+
+}
